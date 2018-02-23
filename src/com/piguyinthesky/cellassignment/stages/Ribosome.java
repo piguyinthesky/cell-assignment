55f194b5b8ea4f735a3e315ef1f34ea97a413b69
piguyinthesky <alexandercai@outlook.com>
2018-02-23 16:41:44 -0700

    finished moving shapes to Ribosome

--- src/com/piguyinthesky/cellassignment/stages/Ribosome.java
+++ src/com/piguyinthesky/cellassignment/stages/Ribosome.java
@@ -19,13 +19,14 @@ public class Ribosome extends Stage {
 	private final int GUANINE = 3;
 	private final int PHOSPHATE = 4;
 	private final int GLUCOSE = 5;
-	
+	private Area[] shapes = new Area[6];
+
 	private final int SQUARE = 0;
 	private final int TRIANGLE = 1;
 	private final int CIRCLE = 2;
 	private final int TRAPEZOID = 3;
 	private final int SMALLSQUARE = 4;
-	private Area[] shapes = new Area[6];
+	private Area[] basicShapes = new Area[6];
 
 	private final int BASEH = CellGame.HEIGHT / 4;
 
@@ -38,14 +39,15 @@ public class Ribosome extends Stage {
 
 	public Ribosome(StageManager sm) {
 		super(sm);
-		
-		createShapes();
+
+		generateBasicShapes();
+		generateShapes();
 		random = new Random();
 
 		for (int i = 0; i < numPairs; i++) {
 			aacids[i] = new AminoAcid(random.nextInt(4), LENGTH * (i * 2 + 1));
 		}
-		
+
 		for (int i = 0; i < bases.length; i++) {
 			if (i % 2 == 0) {
 				bases[i] = new AminoAcid(PHOSPHATE, LENGTH * i);
@@ -55,31 +57,59 @@ public class Ribosome extends Stage {
 		}
 	}
 
-	private void createShapes() {
-		shapes[SQUARE] = new Area(new Rectangle(0, 0, LENGTH, LENGTH));
+	private void generateBasicShapes() {
+		basicShapes[SQUARE] = new Area(new Rectangle(0, 0, LENGTH, LENGTH));
 		int[] trixpoints = { 0, LENGTH, LENGTH / 2 };
 		int[] triypoints = { 0, 0, LENGTH / 2 };
-		shapes[TRIANGLE] = new Area(new Polygon(trixpoints, triypoints, 3));
-		shapes[CIRCLE] = new Area(new Ellipse2D.Double(0, 0, LENGTH, LENGTH));
-		
-		shapes[SMALLSQUARE] = new Area(new Rectangle(0, 0, LENGTH / 3, LENGTH / 3));
+		basicShapes[TRIANGLE] = new Area(new Polygon(trixpoints, triypoints, 3));
+		basicShapes[CIRCLE] = new Area(new Ellipse2D.Double(0, 0, LENGTH, LENGTH));
+
+		basicShapes[SMALLSQUARE] = new Area(new Rectangle(0, 0, LENGTH / 3, LENGTH / 3));
 
 		int[] trapxpoints1 = { 0, LENGTH / 3, LENGTH / 3 };
 		int[] trapxpoints2 = { LENGTH, LENGTH * 2 / 3, LENGTH * 2 / 3 };
 		int[] trapypoints = { 0, 0, LENGTH / 3 };
-		
-		shapes[TRAPEZOID] = new Area(new Polygon(trapxpoints1, trapypoints, 3));
-		shapes[TRAPEZOID].add(new Area(new Polygon(trapxpoints2, trapypoints, 3)));
-		shapes[TRAPEZOID].add(shift(shapes[SMALLSQUARE], LENGTH / 3, 0));
+
+		basicShapes[TRAPEZOID] = new Area(new Polygon(trapxpoints1, trapypoints, 3));
+		basicShapes[TRAPEZOID].add(new Area(new Polygon(trapxpoints2, trapypoints, 3)));
+		basicShapes[TRAPEZOID].add(shift(basicShapes[SMALLSQUARE], LENGTH / 3, 0));
 	}
-	
+
+	private void generateShapes() {
+		shapes[ADENINE] = getShape(SQUARE);
+		shapes[ADENINE].subtract(getShape(TRIANGLE));
+		shapes[ADENINE].add(shift(getShape(CIRCLE), 0, LENGTH / 2));
+
+		shapes[THYMINE] = getShape(SQUARE);
+		shapes[THYMINE].subtract(getShape(TRIANGLE));
+		shapes[THYMINE].add(shift(getShape(SQUARE), 0, LENGTH));
+		shapes[THYMINE].add(shift(getShape(CIRCLE), 0, LENGTH * 3 / 2));
+
+		shapes[CYTOSINE] = getShape(SQUARE);
+		shapes[CYTOSINE].subtract(getShape(TRIANGLE));
+		shapes[CYTOSINE].add(shift(getShape(TRAPEZOID), 0, LENGTH));
+
+		shapes[GUANINE] = getShape(SQUARE);
+		shapes[GUANINE].subtract(getShape(TRIANGLE));
+		shapes[GUANINE].add(shift(getShape(SQUARE), 0, LENGTH));
+		shapes[GUANINE].add(shift(getShape(TRAPEZOID), 0, LENGTH * 2));
+
+		shapes[PHOSPHATE] = getShape(SQUARE);
+		shapes[PHOSPHATE].subtract(shift(getShape(SMALLSQUARE), 0, LENGTH / 3));
+
+		shapes[GLUCOSE] = getShape(SQUARE);
+		shapes[GLUCOSE].add(shift(getShape(TRIANGLE), 0, LENGTH));
+		shapes[GLUCOSE].add(shift(getShape(SMALLSQUARE), -LENGTH / 3, LENGTH / 3));
+		shapes[GLUCOSE].add(shift(getShape(SMALLSQUARE), LENGTH, LENGTH / 3));
+	}
+
 	private Area shift(Area shape, int x, int y) {
 		AffineTransform down = AffineTransform.getTranslateInstance(x, y);
 		return new Area(down.createTransformedShape(shape));
 	}
-	
+
 	private Area getShape(int shape) {
-		return (Area) shapes[shape].clone();
+		return (Area) basicShapes[shape].clone();
 	}
 
 	@Override
@@ -95,7 +125,7 @@ public class Ribosome extends Stage {
 			g.setColor(aacids[i].color);
 			g.fill(aacids[i].bound);
 		}
-		
+
 		for (int i = 0; i < bases.length; i++) {
 			g.setColor(bases[i].color);
 			g.fill(bases[i].bound);
@@ -121,58 +151,29 @@ public class Ribosome extends Stage {
 			switch (id) {
 			case ADENINE:
 				this.color = Color.BLUE;
-
-				Area adenine = getShape(SQUARE);
-				adenine.subtract(getShape(TRIANGLE));
-				adenine.add(shift(getShape(CIRCLE), 0, LENGTH / 2));
-				this.bound = new Area(at.createTransformedShape(adenine));
+				this.bound = new Area(at.createTransformedShape(shapes[ADENINE]));
 				break;
-
 			case THYMINE:
 				this.color = Color.YELLOW;
-
-				Area thymine = getShape(SQUARE);
-				thymine.subtract(getShape(TRIANGLE));
-				thymine.add(shift(getShape(SQUARE), 0, LENGTH));
-				thymine.add(shift(getShape(CIRCLE), 0, LENGTH * 3 / 2));
-				this.bound = new Area(at.createTransformedShape(thymine));
+				this.bound = new Area(at.createTransformedShape(shapes[THYMINE]));
 				break;
-
 			case CYTOSINE:
 				this.color = Color.RED;
-
-				Area cytosine = getShape(SQUARE);
-				cytosine.subtract(getShape(TRIANGLE));
-				cytosine.add(shift(getShape(TRAPEZOID), 0, LENGTH));
-				this.bound = new Area(at.createTransformedShape(cytosine));
+				this.bound = new Area(at.createTransformedShape(shapes[CYTOSINE]));
 				break;
-
 			case GUANINE:
 				this.color = Color.GREEN;
-
-				Area guanine = getShape(SQUARE);
-				guanine.subtract(getShape(TRIANGLE));
-				guanine.add(shift(getShape(SQUARE), 0, LENGTH));
-				guanine.add(shift(getShape(TRAPEZOID), 0, LENGTH * 2));
-				this.bound = new Area(at.createTransformedShape(guanine));
+				this.bound = new Area(at.createTransformedShape(shapes[GUANINE]));
 				break;
-				
 			case PHOSPHATE:
 				this.color = Color.GRAY;
-				
-				Area phosphate = getShape(SQUARE);
-				phosphate.subtract(getShape(SMALLSQUARE));
-
 				at.translate(0, -LENGTH);
-				this.bound = new Area(at.createTransformedShape(phosphate));
+				this.bound = new Area(at.createTransformedShape(shapes[PHOSPHATE]));
 				break;
-				
 			case GLUCOSE:
 				this.color = Color.WHITE;
-				
-				Area glucose = getShape(SQUARE);
-				glucose.add(getShape(TRIANGLE));
-				this.bound = shift(glucose, 0, -LENGTH);
+				at.translate(0, -LENGTH);
+				this.bound = new Area(at.createTransformedShape(shapes[GLUCOSE]));
 				break;
 			}
 		}
